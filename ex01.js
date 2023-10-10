98e3d0788e448e4553baf66b9090af68eb7c53eb
guilhermeoliveirati <guilhermeoliveira.ti@gmail.com>
2023-10-10 00:41:08 -0300

    js

---
+++ ex01.js
@@ -0,0 +1,14 @@
+let text = "A"
+let maior = text.toLocaleUpperCase();
+
+console.log("antes: ", text);
+console.log("depois: ", maior);
+
+if(text==maior){
+    console.log("Sim")
+}
+else{
+    console.log("Não")
+}
+
+console.log(maior);
---
+++ ex02.js
@@ -0,0 +1,28 @@
+let text = "Bom dia";
+
+
+//let list = ["valor 1", "valor 2", "valor 3", "valor 4"];
+
+//console.log(list.length)
+
+//console.log(text.length)
+
+for(let i=0; i <= 10; i=i+1){
+
+    console.log(i*-2)
+
+    // let letraAntes = text[i]; // o
+    // let letraDepois = letraAntes.toLocaleUpperCase(); // O
+
+    // if(letraAntes === " "){ // 
+    //     console.log("erro, letra vazia ", letraAntes);
+    //     continue;
+    // }
+     
+    // if (letraAntes == letraDepois){ // o == O
+    //     console.log("Sim, a letra é maior, a letra é: "+ letraAntes)
+    // }
+    // else{
+    //     console.log("Não, a letra é menor, a letra é: "+ letraAntes )
+    // }
+}
