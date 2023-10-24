e224fe416bb7d2c886f62bd69352d56e8b19ad4b
Guilherme Oliveira <guilhermeoliveira.ti@gmail.com>
2023-10-24 16:52:21 -0300

    exe10

--- Exercicios/ex008.js
+++ Exercicios/ex008.js
@@ -1,1 +1,6 @@
-console.log('O console funcionou corretamente!')
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {// condição simples!
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO`)
+}
+console.log(`Dirija sempre usando o cinto de segurança`)
