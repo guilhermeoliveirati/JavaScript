8ef73a8e98ff03c6398421448e9efc4a4f2c70c7
guilhermeoliveirati <guilhermeoliveira.ti@gmail.com>
2023-11-24 01:07:19 -0300

    exercicio de repetição

---
+++ Exercicios/ambiente teste.js
@@ -0,0 +1,16 @@
+/*
+Teste no começo
+var c = 1
+while (c <= 6) {
+    console.log(`olá ${c}`)
+    c++ // c = c + 1
+}
+*/
+
+//-----------------------
+
+console.log('Inicio')
+for(var c=1; c<=5; c++) {
+    console.log(c)
+}
+console.log('Fim')
---
+++ Exercicios/ambiente.js
@@ -0,0 +1,15 @@
+/*
+Teste no começo
+var c = 1
+while (c <= 6) {
+    console.log(`olá ${c}`)
+    c++ // c = c + 1
+}
+*/
+
+//Teste no fim
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ //c = c + 1
+} while (c <= 6)
