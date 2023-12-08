f5cf2242d47fb2f96ecdcbab8aac1970ca0f7743
guilhermeoliveirati <guilhermeoliveira.ti@gmail.com>
2023-12-08 05:00:41 -0300

    Finalizando JavaScript

---
+++ Exercicios/Ambiente/ambiente015.js
@@ -0,0 +1,12 @@
+let num = [5,8,4]
+num[3] = 6 //Para adicionar mais um elemento
+num.push(7) //Para adicionar mais um elemnto no ultimo valor
+num.length //Para saber o cumprimento
+num.sort() //Para organizar
+
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`------------------------------------`)
+console.log(`Valor dos vetores: ${num}`)
+console.log(`------------------------------------`)
+console.log(`O primeiro valor do vetor é: ${num[0]}`)
+console.log(`------------------------------------`)
---
+++ Exercicios/Funcao/funcao01.js
@@ -0,0 +1,16 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+
+/*
+let res = parimpar(3)
+console.log(res)
+*/
+
+// ou
+
+console.log(parimpar(223))
